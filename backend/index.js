98ffbfba22339fa3f983494e7e292f221186e72c
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-13 21:22:41 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,9 @@ const app = express()
 app.get("/",(req,res) => {
     res.send("server is ready")
 })
+
+const port = process.env.PORT || 3000
+
+app.listen(port,() => {
+    console.log(`serve at http://localhost:${port}`)
+}) 
