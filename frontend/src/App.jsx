7c1baddf0e59f82ec7fa84add62ab575544ba868
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-13 21:46:03 +0530

    fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 // import React from 'react'
 
 const App = () => {
+
+
+  const getUser = () => {
+    fetch("http://localhost:3000/api/user")
+    .then(res => res.json())
+    .then(json => console.log(json))
+  }
+
+
   return (
     <div>
       app
